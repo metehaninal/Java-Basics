9c9548d024c077d3662f87f65fbc18c6d6ae3c37
Metehan İNAL <metehan.inal25@gmail.com>
2022-06-23 23:07:02 +0300

    added

---
+++ Main.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+    
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Mat:");
+            mat = input.nextInt();
+
+            System.out.print("Fizik:");
+            fizik = input.nextInt();
+
+            System.out.print("Kimya:");
+            kimya = input.nextInt();
+
+            System.out.print("Turkce:");
+            turkce = input.nextInt();
+
+            System.out.print("Tarih:");
+            tarih = input.nextInt();
+
+            System.out.print("Muzik:");
+            muzik = input.nextInt();
+        }
+        
+        double average = (mat+fizik+kimya+turkce)/6;
+        String s = (average<60) ? "sinifi gecti" : "sinifi gecemedi";
+        System.out.println(s);
+        
+    }
+}
