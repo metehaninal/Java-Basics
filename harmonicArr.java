412659bcc43a6bea4788e4f9ec136195f2596e2b
Metehan İNAL <metehan.inal25@gmail.com>
2022-07-30 17:16:10 +0300

    harmonic av. of array

---
+++ harmonicArr.java
@@ -0,0 +1,12 @@
+public class arrAve {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double total=0;
+        for (int i = 0; i < numbers.length; i++) {
+            total += 1.0/numbers[i];
+        }
+
+        System.out.println("result is: " + total);
+        
+    }
+}
