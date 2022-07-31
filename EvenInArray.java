be42064fa22f7115e3c480f2729f206211b66bdd
Metehan İNAL <metehan.inal25@gmail.com>
2022-07-31 12:03:49 +0300

    repeated even numbers

---
+++ EvenInArray.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class EvenInArray {
+
+    static boolean isFind(int[] arr,int value){
+        for (int i : arr) {
+            if(i == value){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,3,41,2,3,1,54,2,6,15,6};
+        int dublicates[] = new int[arr.length];
+        int startingIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if((i != j) && (arr[i] == arr[j]) && (arr[i] % 2 == 0)){
+                    if(!isFind(dublicates, arr[i])){
+                        dublicates[startingIndex++] = arr[i];
+                    }   
+                    break;
+                }
+            }
+        }
+
+        for (int i : dublicates) {
+            if(i != 0){
+                System.out.print(i+ " ");
+            }
+        }
+        
+    }
+}
